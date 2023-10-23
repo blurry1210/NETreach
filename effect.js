701fa5ec2b324136401cba8ebccc2860d8e9b3ac
Alex Borza <135742922+blurry1210@users.noreply.github.com>
2023-10-23 14:18:06 +0300

    ad

--- effect.js
+++ effect.js
@@ -1,17 +1,22 @@
-const imageFrame = document.getElementById("imageFrame");
-const images = document.querySelectorAll(".image-container");
+    const codeBlock = document.querySelector('.code-block');
+    const codeLines = [
+        'const imageFrame = document.getElementById("imageFrame");',
+        'const images = document.querySelectorAll(".image-container");',
+        'const maxDistance = 100; // Maximum distance from the center',
+        'imageFrame.addEventListener("mousemove", (e) => {',
+        '    const centerX = imageFrame.clientWidth / 2;',
+        '    const centerY = imageFrame.clientHeight / 2;',
+        // ... Add more lines of code
+    ];
 
-imageFrame.addEventListener("mousemove", (e) => {
-    const centerX = imageFrame.clientWidth / 2;
-    const centerY = imageFrame.clientHeight / 2;
+    let currentIndex = 0;
 
-    images.forEach((image, index) => {
-        const angle = (Math.PI * 2) * (index / images.length);
-        const distance = 100; // Adjust this value to control the distance from the center
+    function displayNextLine() {
+        if (currentIndex < codeLines.length) {
+            codeBlock.textContent += codeLines[currentIndex] + '\n';
+            currentIndex++;
+            setTimeout(displayNextLine, 1000); // Delay between lines (adjust as needed)
+        }
+    }
 
-        const moveX = distance * Math.cos(angle);
-        const moveY = distance * Math.sin(angle);
-
-        image.style.transform = `translate(${moveX}px, ${moveY}px)`;
-    });
-});
+    displayNextLine();
