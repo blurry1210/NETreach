9706f33a3ae5df9890a106498d422462cf65d898
Alex Borza <135742922+blurry1210@users.noreply.github.com>
2023-10-30 17:02:41 +0200

    update servicii

--- srv.js
+++ srv.js
@@ -1,22 +1,30 @@
 function handleScroll() {
     const services = document.querySelectorAll('.service');
     services.forEach((service) => {
-        const rect = service.getBoundingClientRect();
-        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-            if (!service.classList.contains('visible')) {
-                service.style.top = '0';
-                service.style.opacity = 1;
-                setTimeout(() => {
-                    const description = service.querySelector('p');
-                    description.style.opacity = 1;
-                }, 2000);
-                service.classList.add('visible');
-            }
+      const rect = service.getBoundingClientRect();
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        if (!service.classList.contains('visible')) {
+          service.style.top = '0';
+          service.style.opacity = 1;
+  
+          // Apply fade-in animation to the image
+          const image = service.querySelector('img');
+          image.style.transition = 'opacity 1s ease-in';
+          image.style.opacity = 1;
+  
+          setTimeout(() => {
+            const description = service.querySelector('p');
+            description.style.opacity = 1;
+          }, 2000);
+  
+          service.classList.add('visible');
         }
+      }
     });
-}
-
-window.addEventListener('scroll', handleScroll);
-
-// Initial check for elements in the viewport
-handleScroll();
+  }
+  
+  window.addEventListener('scroll', handleScroll);
+  
+  // Initial check for elements in the viewport
+  handleScroll();
+  
--- title.js
+++ title.js
@@ -1,39 +1,28 @@
-function createCoolTitle(title) {
-    // Create a cool title element
-    const titleElement = document.createElement('h1');
-    titleElement.textContent = title;
-    titleElement.classList.add('cool-title');
-    document.querySelector('.title-container').appendChild(titleElement);
+// JavaScript to animate titles when they are in the viewport
 
-    // Apply animation and font styles
-    titleElement.style.fontFamily = 'cursive'; // Set a cool font
-    titleElement.style.fontSize = '36px'; // Set the font size
-    titleElement.style.color = '#4a90e2'; // Set the text color
-
-    // Add an animation
-    titleElement.style.animation = 'fadeIn 2s ease';
-
-    // Define the fadeIn animation
-    const keyframes = `
-        @keyframes fadeIn {
-            from {
-                opacity: 0;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-    `;
-
-    // Create a style element for the animation
-    const styleElement = document.createElement('style');
-    styleElement.textContent = keyframes;
-
-    // Append the style element to the document's head
-    document.head.appendChild(styleElement);
-}
-
-// Call the createCoolTitle function with different titles
-createCoolTitle('Service 1 Title');
-createCoolTitle('Service 2 Title');
-createCoolTitle('Service 3 Title');
+function checkIfElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+  
+    if (rect.top >= 0 && rect.bottom <= windowHeight) {
+      element.classList.add('in-viewport');
+    } else {
+      element.classList.remove('in-viewport');
+    }
+  }
+  
+  // Get all elements with the class "animated-title"
+  const titleElements = document.querySelectorAll('.animated-title');
+  
+  function handleElementsInViewport() {
+    titleElements.forEach((element) => {
+      checkIfElementInViewport(element);
+    });
+  }
+  
+  window.addEventListener('scroll', handleElementsInViewport);
+  window.addEventListener('resize', handleElementsInViewport);
+  
+  // Initial check on page load
+  handleElementsInViewport();
+  
