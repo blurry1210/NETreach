94bbde70ca6f397c0b478c2fb1e208278056efa5
DeianGG <49059344+DeianGG@users.noreply.github.com>
2023-10-31 13:33:20 +0200

    servicii design

--- srv.js
+++ srv.js
@@ -2,20 +2,19 @@ function handleScroll() {
     const services = document.querySelectorAll('.service');
     services.forEach((service) => {
       const rect = service.getBoundingClientRect();
-      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight+500) {
         if (!service.classList.contains('visible')) {
           service.style.top = '0';
           service.style.opacity = 1;
-  
           // Apply fade-in animation to the image
-          const image = service.querySelector('img');
+          const image = service.querySelector('.service1__image');
           image.style.transition = 'opacity 1s ease-in';
           image.style.opacity = 1;
   
           setTimeout(() => {
-            const description = service.querySelector('p');
+            const description = service.querySelector('.service1__content');
             description.style.opacity = 1;
-          }, 2000);
+          }, 1000);
   
           service.classList.add('visible');
         }
